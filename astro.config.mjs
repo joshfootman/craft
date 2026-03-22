18478436f9cf6c57febba02176015105c5506e65
joshjfootman <joshjfootman@gmail.com>
2026-03-22 10:36:48 +0000

    add react, tailwind, oxc

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,12 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-// https://astro.build/config
-export default defineConfig({});
+import react from "@astrojs/react";
+
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineConfig({
+  integrations: [react()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
