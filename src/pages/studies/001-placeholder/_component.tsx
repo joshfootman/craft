753695193d32dac6a46f2d3999c5e0708ec6a580
joshjfootman <joshjfootman@gmail.com>
2026-03-23 20:52:48 +0000

    remove placeholder

--- src/pages/studies/001-placeholder/_component.tsx
+++
@@ -1,7 +0,0 @@
-export default function Placeholder() {
-  return (
-    <div className="flex h-full items-center justify-center">
-      <p className="text-lg text-muted-foreground">001-placeholder</p>
-    </div>
-  );
-}
