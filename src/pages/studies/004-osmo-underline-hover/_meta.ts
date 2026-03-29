8c5be54e226fdc20216b69bf811f93e31ac3651a
joshjfootman <joshjfootman@gmail.com>
2026-03-29 12:22:34 +0100

    add study

---
+++ src/pages/studies/004-osmo-underline-hover/_meta.ts
@@ -0,0 +1,14 @@
+import type { Meta } from "~/types/study";
+
+export const meta = {
+  id: "004-osmo-underline-hover",
+  title: "Underline Hover",
+  description: "Underline hover animation",
+  status: "published",
+  tags: ["CSS"],
+  category: "Osmo Side Navigation",
+  date: "2026-03-29",
+  theme: "light",
+  viewport: "any",
+  inspiration: ["https://codepen.io/osmosupply/full/JoPPOXj"],
+} satisfies Meta;
